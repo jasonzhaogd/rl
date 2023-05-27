6f45340fa07e9d1c28cbb96dbdf5da0a2d0e4e7e
Jason <jasonzhaogd@gmail.com>
2023-05-27 08:52:38 +0800

    main course done

--- Nicholas-Renotte/learning_in_3_hours.py
+++ Nicholas-Renotte/learning_in_3_hours.py
@@ -10,25 +10,25 @@ env = gym.make('CartPole-v0')
 # print(env.observation_space)
 
 # test out the env
-# episodes = 5
-# for episode in range(1, episodes + 1):
-#     state = env.reset()
-#     done = False
-#     score = 0
+episodes = 5
+for episode in range(1, episodes + 1):
+    state = env.reset()
+    done = False
+    score = 0
     
-#     while not done:
-#         env.render()
-#         action = env.action_space.sample()
-#         n_state, reward, done, info = env.step(action)
-#         score += reward
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score += reward
         
-#     print('Episode:{}, Score:{}'.format(episode, score))
+    print('Episode:{}, Score:{}'.format(episode, score))
     
-# env.close()
+env.close()
 
 
 # training model
-log_path = os.path.join('training', 'logs')
-env = DummyVecEnv([lambda: env]) # type: ignore
-model = PPO('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=20000)
+# log_path = os.path.join('training', 'logs')
+# env = DummyVecEnv([lambda: env]) # type: ignore
+# model = PPO('MlpPolicy', env, verbose=1)
+# model.learn(total_timesteps=20000)
