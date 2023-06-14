8c7ed2ab75575f2984e9e884c564b62f04abfd16
Jason <jasonzhaogd@gmail.com>
2023-06-14 23:56:33 +0800

    04/01_CartPole.py

--- Hands-On/04/01_cartpole.py
+++ Hands-On/04/01_cartpole.py
@@ -26,9 +26,53 @@ class Net(nn.Module):
 Episode = namedtuple('Episode', field_names=['reward', 'steps'])
 EpisodeStep = namedtuple('EpisodeStep', field_names=['observation', 'action'])
 
+def iterate_batch(env, net, batch_size):
+    batch = []
+    episode_reward = 0.0
+    episode_steps = []
+    obs = env.reset()
+    softmax = nn.Softmax(dim=1)
+    while True:
+        obs_v = torch.FloatTensor([obs])
+        act_probs_v = softmax(net(obs_v))
+        act_probs = act_probs_v.data.numpy()[0]
+        action = np.random.choice(len(act_probs), p=act_probs)
+        next_obs, reward, is_done, _ = env.step(action)
+        episode_reward += reward
+        step = EpisodeStep(observation=obs, action=action)
+        episode_steps.append(step)
+        if is_done:
+            e = Episode(reward=episode_reward, steps=episode_steps)
+            batch.append(e)
+            episode_reward = 0.0
+            episode_steps = []
+            next_obs = env.reset()
+            if batch_size == len(batch):
+                yield batch
+                batch = []
+        obs = next_obs
+
+def filter_batch(batch, percentile):
+    rewards = list(map(lambda s:s.reward, batch))
+    reward_bound = np.percentile(rewards, percentile)
+    reward_mean = float(np.mean(rewards))
+    
+    train_obs = []
+    train_act = []
+    
+    for reward, steps in batch:
+        if reward < reward_bound:
+            continue
+        train_obs.extend(map(lambda s:s.observation, steps))
+        train_act.extend(map(lambda s:s.action, steps))
+    
+    train_obs_v = torch.FloatTensor(train_obs)
+    train_act_v = torch.LongTensor(train_act)
+    return train_obs_v, train_act_v, reward_bound, reward_mean
+
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
-    obs_size = env.observation_space[0]
+    obs_size = env.observation_space.shape[0]
     n_actions = env.action_space.n
     
     net = Net(obs_size, HIDDEN_SIZE, n_actions)
@@ -36,4 +80,21 @@ if __name__ == "__main__":
     optimizer = optim.Adam(params=net.parameters(), lr=0.01)
     write = SummaryWriter(comment="-CartPole")
     
-    for i, batch in enumerate(iterate_ba)
+    # print(env.reset())
+    
+    for i, batch in enumerate(iterate_batch(env, net, BATCH_SIZE)):
+        obs_v, act_v, reward_b, reward_m = filter_batch(batch, PERCENTIL)
+        optimizer.zero_grad()
+        action_score_v = net(obs_v)
+        loss = objective(action_score_v, act_v)
+        loss.backward()
+        optimizer.step()
+        print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f" % (i, loss.item(), reward_m, reward_b))
+        write.add_scalar("loss", loss.item(), i)
+        write.add_scalar("reward_bound", reward_b, i)
+        write.add_scalar("reward_mean", reward_m, i)
+        if reward_m > 199:
+            print("Solved!")
+            break
+        
+    write.close()
